c5f4fdd48458e2f9850efda515e288e9e0b3059b
ViniciusRib1 <viniciusribeiro0709@gmail.com>
2024-01-27 08:31:25 -0300

    Colocando os arquivos html e separando a pasta do logado e não logado

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-app.use(express.static('public/html'));
+app.use(express.static('public'));
 
 app.listen(PORT, () => {
   console.log(`Servidor rodando em http://localhost:${PORT}`);
