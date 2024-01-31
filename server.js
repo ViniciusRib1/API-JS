d71533fd34703de6613bae2be8374cee554720d1
ViniciusRib1 <viniciusribeiro0709@gmail.com>
2024-01-31 14:55:39 -0300

    Melhorando as pastas e alterando o front

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-app.use(express.static('public'));
+app.use(express.static('view/Html'));
 
 app.listen(PORT, () => {
   console.log(`Servidor rodando em http://localhost:${PORT}`);
