c788042c5dc2b94f9151b8898a5855f6edf7e744
ViniciusRib1 <viniciusribeiro0709@gmail.com>
2024-05-28 19:42:36 -0300

    Alterando algumas coisas

--- src/server.js
+++ src/server.js
@@ -1,8 +1,12 @@
 const express = require('express');
 const app = express();
+const pasth = require("path")
 const PORT = process.env.PORT || 3000;
 
 app.use(express.static('view/Html'));
+app.use(express.static('view/Css'));
+
+
 
 app.listen(PORT, () => {
   console.log(`Servidor rodando em http://localhost:${PORT}`);
